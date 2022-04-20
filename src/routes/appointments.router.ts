633243d83bb8472c64ab8f8f2444d24565222095
wellison santos <silva.s.wellison@gmail.com>
2022-04-20 18:35:47 -0300

    crianção do model Appointment

--- src/routes/appointments.router.ts
+++ src/routes/appointments.router.ts
@@ -1,21 +1,25 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const parseDate = startOfHour(parseISO(date))
+  const parseDate = startOfHour(parseISO(date));
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    parseDate,
-  };
+  const timeConflict = appointments.find(appointment =>
+    isEqual(parseDate, appointment.date),
+  );
+
+  if (timeConflict) {
+    return response.status(400).json({ message: 'Error conflito' });
+  }
+
+  const appointment = new Appointment(provider, parseDate);
 
   appointments.push(appointment);
 
