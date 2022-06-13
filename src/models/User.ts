175cf732af7323392f013b120fba5198634c79fe
Wellison Santos <silva.s.wellison@gmail.com>
2022-06-13 14:21:56 -0300

    upload de imagem

--- src/models/User.ts
+++ src/models/User.ts
@@ -18,11 +18,7 @@ class User {
   @Column()
   email: string;
 
-<<<<<<< HEAD
   @Column()
-=======
-  @Column({ select: true })
->>>>>>> 4e927318945250ca6b992e8a787eef6a616f2261
   password: string;
 
   @CreateDateColumn()
